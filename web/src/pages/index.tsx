0fd50fd9ba16e5fd1f7f0842ab31f37f253df0f7
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-10-29 06:37:47 -0300

    chore: adicionando chakra-ui

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+   <Text>testando 123</Text>
+  )
+}
+
+export default Home
